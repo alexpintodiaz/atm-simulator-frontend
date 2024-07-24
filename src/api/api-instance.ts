65384d88cd7c44303ba4ea1e0da03cd787696e8c
alexpintodiaz <alexanderp143@hotmail.com>
2024-07-24 01:20:04 -0500

    add axios instance

---
+++ src/api/api-instance.ts
@@ -0,0 +1,38 @@
+
+import axios from 'axios'
+
+const baseURL = process.env.ATM_API_URL
+
+
+type FetchDataFuncProps = {
+  endpoint: string
+  method: 'GET' | 'POST' | 'DELETE' | 'PUT'
+  payload?: any
+  params?: any
+  responseType?: 'json' | 'arraybuffer'
+}
+
+export const atmApi = axios.create({
+  baseURL,
+  timeout: 1000 * 80,
+  headers: {
+    'Content-Type': 'application/json',
+    accept: 'application/json',
+  },
+})
+
+export const fetchApi = <T>({
+  method,
+  endpoint,
+  payload,
+  params,
+  responseType = 'json',
+}: FetchDataFuncProps) => {
+  return atmApi.request<T>({
+    method,
+    url: endpoint,
+    data: payload,
+    params,
+    responseType,
+  })
+}
