d0d0c825ed8f2cee16a9826a70ec6334b93c6e29
alexpintodiaz <alexanderp143@hotmail.com>
2024-07-23 22:34:36 -0500

    confif lintern and prettier

--- src/ui/header.tsx
+++ src/ui/header.tsx
@@ -1,9 +1,9 @@
-import { FC } from "react";
+import { FC } from 'react'
 
 export const Header: FC = () => {
   return (
-    <header className="flex w-full bg-[#6183a1] h-[80px] px-10 py-4">
+    <header className='flex w-full bg-[#6183a1] h-[80px] px-10 py-4'>
       <span>ATM Simulator</span>
     </header>
-  );
-};
+  )
+}
