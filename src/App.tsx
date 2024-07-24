612b5877465865b7a30b751d4134fe36b8b8a426
alexpintodiaz <alexanderp143@hotmail.com>
2024-07-23 19:33:13 -0500

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-zinc-900 h-screen text-white flex items-center justify-center">
+      <div className="bg-gray-500 p-4 w-2/5">
+        <h1 className="text-3xl font-bold text-center block my-2">ATM Simulator</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App
