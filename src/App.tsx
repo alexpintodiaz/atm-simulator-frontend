6eef633e254c2187ffba6d5cfd3e30846d179c4e
alexpintodiaz <alexanderp143@hotmail.com>
2024-07-24 19:59:57 -0500

    dev test

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
       <Header />
       <div className='bg-gray-500 p-4 w-2/5'>
         <h1 className='text-3xl font-bold text-center block my-2'>
-          ATM Simulator
+          ATM Simulator DEVV
         </h1>
         <div className='flex justify-around'>
           <ActionButton text='Enter' />
