c303a2419c245ef236dfb3c92af2aecd434b95f8
alexpintodiaz <alexanderp143@hotmail.com>
2024-07-24 22:08:35 -0500

    navigate test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { ActionButton } from './components/action-button'
 import { UserForm } from './components/user-form'
+import { useAppNavigate } from './hooks/use-app-navigate'
 import { useUserStore } from './store/user-store'
 
 function App() {
   const { getUsers } = useUserStore()
+  const navigate = useAppNavigate()
 
   getUsers()
 
@@ -11,11 +13,14 @@ function App() {
     <div className='bg-zinc-900 h-screen text-white flex flex-col items-center justify-between'>
       <div className='bg-gray-500 p-4 w-2/5'>
         <h1 className='text-3xl font-bold text-center block my-2'>
-          ATM Simulator
+          ATM Simulator_d
         </h1>
         <div className='flex justify-around'>
           <ActionButton text='Enter' />
-          <ActionButton text='Create Account' />
+          <ActionButton
+            text='Create Account'
+            onClick={() => navigate('/dashboard')}
+          />
         </div>
 
         <UserForm />
